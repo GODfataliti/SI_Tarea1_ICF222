25000e3f03b85ad1f84d702382d7ca45750de150
Fataliti <hell.jnacho@hotmail.com>
2021-09-08 17:38:26 -0300

    estructura del archivo interfaz

--- code/fata/interfaz.py
+++ code/fata/interfaz.py
@@ -5,16 +5,54 @@ import numpy as np
 import os
 import dotenv
 
+load_dotenv()
 
+BLACK = os.getenv('BLACK')
+WHITE = os.getenv('WHITE')
+BACKGROUND = os.getenv('BACKGROUND')
+GREEN = os.getenv('GREEN')
+LINEABG = os.getenv('LINEABG')
+DIMENSION_VOL = int(os.getenv('DIMENSION_VOL'))
+
+#Dar a elegir el color al jugador
+def elegir_color():
+    pass
+
+
+#Clase para dibujar el tablero
 class gridReversi:
 
-    def __init__(self) -> None:
+    def __init__(self):
+        self.griid = []
+        self.jugador = None
+        self.adversario = None
+    
+    def generar_grid(self):
+        pass
+    
+    def rellener_grid(self):
         pass
 
+    def contar_color(self):
+        pass
 
+    def movimiento(self):
+        pass
 
-
+#Clase que ejecuta la aplicacion GUI
 class Interfaz:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        pygame.init()
+
+
+
+
+
+
+#Main
+def main():
+    Interfaz()
+
+if __name__ == '__main__':
+    main()
